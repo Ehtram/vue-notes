732f59590fc51ff0eb251164a8c2ad85322b0659
Quentin Laplanche <quentin.laplanche@outlook.fr>
2016-11-04 07:52:55 +0100

    Added Material Design Lite + some more component

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="notes">
-    <p>Notes Component:</p>
-    <p> task : {{task}}</p>
+    <h1>{{listName}}</h1>
+    <ul>
+      <li v-for="todo in tasks" class="mdl-list__item">
+        <span class="mdl-list__item-primary-content">
+          <i class="material-icons mdl-list__item-avatar">person</i>
+          {{todo.name}}
+        </span>
+      </li>
+    </ul>
+    <button class="mdl-button mdl-js-button mdl-js-ripple-effect" v-on:click="add">
+      Add a tdodo
+    </button>
   </div>
 </template>
 
@@ -10,11 +20,21 @@ export default {
   name: 'notes',
   data() {
     return {
-      task: 'Super task to do this afternoon',
+      tasks: [
+        { name: 'Super task to do this afternoon' },
+        { name: 'Super task to do tomorrow' },
+      ],
+      listName: 'Todos',
     };
+  },
+  methods: {
+    add() {
+      console.log('Add Event sent from the button to the notes component');
+    },
   },
 };
 </script>
 
 <style lang="css" scoped>
+
 </style>
